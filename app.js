eee2444cad723f9e530e5558f3728c65278c0dc8
Harshit Ruwali <harshitruwali@outlook.com>
2020-06-09 00:50:16 +0530

    Working in the sign up page

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
-	res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/login.html')
+});
+
+app.post('/', function(req, res){
+	var phoneNumber = req.body.phoneNumber;
+	console.log(phoneNumber);
+
 });
 
 
